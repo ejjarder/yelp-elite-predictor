ede1f6d2028cfca5f38bec9e6b5580ff96ebbba2
ejjarder <suaveh@gmail.com>
2015-11-10 17:21:38 +0800

    rename predict file to model, add new predict that actually contains prediction functions

---
+++ model.elite.count.R
@@ -0,0 +1,55 @@
+library(caret)
+library(glmnet)
+
+non.predictors <- c('X', # index
+                    'type', # everything is a user
+                    'user_id', 'name', # identifiers
+                    'yelping_since', # replaced by days.yelping
+                    'was.elite', 'elite.count') # results
+
+read.user.dataset <- function() {
+    user.dataset <- read.csv('user.data.csv')
+    user.dataset[is.na(user.dataset)] <- 0
+    user.dataset$was.elite <- as.factor(user.dataset$elite.count > 0)
+    user.dataset
+}
+
+get.formula <- function(response, predictors) {
+    as.formula(paste(response, '~', paste(predictors, collapse = ' + ')))
+}
+
+train.glm.model <- function(family, response, predictors, data) {
+    print(sprintf('Predicting %s using %s', response, family))
+    
+    train(get.formula(response, predictors),
+          data = data[c(response, predictors)], method = 'glm',
+          family = family,
+          trControl = trainControl(method = 'cv', verboseIter = TRUE))
+}
+
+get.model.per.family <- function(response, model.definition, possible.predictors,
+                                 training.set) {
+    glm.families <- model.definition[[response]]$glm.families
+    models.per.family <- lapply(glm.families, train.glm.model, response,
+                                possible.predictors, training.set)
+    names(models.per.family) <- glm.families
+    models.per.family
+}
+
+get.models.per.response <- function(model.definition, possible.predictors,
+                                    user.training.set) {
+    response.list <- names(model.definition)
+    models <- lapply(response.list, get.model.per.family, model.definition,
+                     possible.predictors, user.training.set)
+    names(models) <- response.list
+    models
+}
+
+get.models <- function(training.set) {
+    column.names <- names(training.set)
+    possible.predictors.bool <- !(column.names %in% non.predictors)
+    possible.predictors <- column.names[possible.predictors.bool]
+    
+    get.models.per.response(model.definition, possible.predictors,
+                            user.training.set)
+}
--- predict.elite.count.R
+++ predict.elite.count.R
@@ -1,85 +1,23 @@
-library(caret)
-library(glmnet)
-
-set.seed(59473717)
-
-P.THRESHOLD <- 0.05
-non.predictors <- c('X', # index
-                     'type', # everything is a user
-                     'user_id', 'name', # identifiers
-                     'yelping_since', # replaced by days.yelping
-                     'was.elite', 'elite.count') # results
-
-read.user.dataset <- function() {
-    user.dataset <- read.csv('user.data.csv')
-    user.dataset[is.na(user.dataset)] <- 0
-    user.dataset$was.elite <- as.factor(user.dataset$elite.count > 0)
-    user.dataset
+validate.binomial <- function(prediction, expected) {
+    confusionMatrix(prediction, expected)
 }
 
-get.formula <- function(response, predictors) {
-    as.formula(paste(response, '~', paste(predictors, collapse = ' + ')))
+predict.per.model <- function(model, response, validation.function, data) {
+    prediction <- predict(model, data)
+    validation <- validation.function(prediction, data[[response]])
+    list(prediction = prediction, validation = validation)
 }
 
-get.glm.model <- function(response, predictors, data, family) {
-    glm(get.formula(response, predictors), data,
-        family = family)
+predict.per.response <- function(response, models, model.definition, data) {
+    validation.function <- model.definition[[response]]$validation.function
+    response.models <- models[[response]]
+    prediction.per.model <- lapply(response.models, predict.per.model,
+                                   response, validation.function, data)
 }
 
-find.coefficient.data <- function(response, predictors, data, family) {
-    current.fit <- get.glm.model(response, predictors, data, family)
-    coefficients <- summary(current.fit)$coefficients
-    list(coefficients = coefficients,
-         max.p = tail(sort(coefficients[-1, 4]), 1))
+get.predictions <- function(models, model.definition, data) {
+    predictions <- lapply(names(models), predict.per.response,
+                          models, model.definition, data)
+    names(predictions) <- names(models)
+    predictions
 }
-
-get.predictors <- function(response, predictors, data, family) {
-    print(sprintf('Looking for predictors for %s using %s',
-                  response, family))
-    
-    while (length(predictors) > 0) {
-        coef.data <- find.coefficient.data(response, predictors, data, family)
-        
-        if (coef.data$max.p > P.THRESHOLD) {
-            predictors <- predictors[predictors != names(coef.data$max.p)]
-        } else break
-    }
-    
-    predictors
-}
-
-get.possible.predictors <- function(column.names) {
-    possible.predictors.bool <- !(column.names %in% non.predictors)
-    column.names[possible.predictors.bool]
-}
-
-train.glm.model <- function(response, predictors, data, family) {
-    glm.predictors <- get.predictors(response, predictors, data, family)
-    
-    print('Predictors found!')
-    print(glm.predictors)
-    print('Training Model...')
-    
-    train(get.formula(response, glm.predictors),
-          data = data[c(response, glm.predictors)], method = 'glm',
-          family = family)
-}
-
-user.dataset <- read.user.dataset()
-
-in.training <-
-    createDataPartition(user.dataset$elite.count, p = 0.6, list = FALSE)
-
-user.training.set <- user.dataset[in.training, ]
-user.testing.set <- user.dataset[-in.training, ]
-
-possible.predictors <- get.possible.predictors(names(user.training.set))
-
-poisson.model <- train.glm.model('elite.count', possible.predictors,
-                                 user.training.set, 'poisson')
-
-quasipoisson.model <- train.glm.model('elite.count', possible.predictors,
-                                      user.training.set, 'quasipoisson')
-
-quasibinomial.model <- train.glm.model('was.elite', possible.predictors,
-                                       user.training.set, 'quasibinomial')
